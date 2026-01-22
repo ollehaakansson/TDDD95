98bd0a62f4ff69f646b5582817a42878c1c7232b
Olle <ollesplugg@gmail.com>
2026-01-22 13:16:29 +0100

    Mall

---
+++ 1_exercise/help.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstudio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>   
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <utility
+#include <vector>
+
+using namespace std;
+typedef vector <int> vi;
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
