be465c317ef90ea1148ae6cd22047a6a38548072
ollehaakansson <olleh@OllesPL.localdomain>
2026-01-23 11:50:45 +0100

    tweaks

--- 1_exercise/ljutnja/ljutnja.cpp
+++ 1_exercise/ljutnja/ljutnja.cpp
@@ -15,15 +15,15 @@ int main(int argc, char* argv[])
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    long long M; // Totalt antal godis
+    unsigned long long M; // Totalt antal godis
     int N; // Antal barn
 
     // läser in M och N
     if (!(cin >> M >> N)) return 0;
 
     // Använder vektor för att lagra önskemålen
-    vector<long long> wishes(N);
-    long long total_wishes = 0;
+    vector<unsigned long long> wishes(N);
+    unsigned long long total_wishes = 0;
 
     //Läs in önskemålen
     for (int i = 0; i < N; i++) {
@@ -31,20 +31,20 @@ int main(int argc, char* argv[])
         total_wishes += wishes[i];
     }
     // Räkna totalt underskott 
-    long long total_missing = total_wishes - M;
+    unsigned long long total_missing = total_wishes - M;
     if(total_missing <= 0) total_missing = 0;
 
     // Sortera önskemålen i stigande ordning 
     sort(wishes.begin(), wishes.end());
 
-    long long anger = 0;
+    unsigned long long anger = 0;
 
     // Iterera genom barnen och bestäm hur mycket vi ska ta från varje
     for (int i = 0; i < N; i++) {
-        long long remaining_children = N - i;
-        long long fair_share_missing = total_missing / remaining_children;
+        unsigned long long remaining_children = N - i;
+        unsigned long long fair_share_missing = total_missing / remaining_children;
 
-        long long take_from_child;
+        unsigned long long take_from_child;
         if (wishes[i] < fair_share_missing) { // Barnet önskar sig mindre än fair share
             take_from_child = wishes[i];
         } else { // Vi tar bara vår fair share från detta barn
